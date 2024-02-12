36e441099d4f5ee5051edad701737c53fce172ae
bahnarupetronel <79132145+bahnarupetronel@users.noreply.github.com>
2024-02-12 10:01:22 +0200

    add results button
    add results button in vote page to be easier for the user to access the page

--- frontend/app/_components/election/ElectionTitle.tsx
+++ frontend/app/_components/election/ElectionTitle.tsx
@@ -1,21 +1,34 @@
 import { ElectionModel } from "@/_interfaces/election.model";
 import styles from "./election.module.css";
 import dayjs from "dayjs";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const ElectionTitle = ({ election }: { election: ElectionModel }) => {
   const year = dayjs(election?.startDate).get("year");
-
+  const pathname = usePathname();
+  const route = pathname.split("/")[1];
   if (!election) return <h2></h2>;
 
   return (
-    <h2 className={styles["title"]}>
-      {election?.type.name === "Referendum" ? (
-        election?.type.name
-      ) : (
-        <>Alegeri {election?.type.name}</>
+    <header className={styles["header-event"]}>
+      <h2 className={styles["title"]}>
+        {election?.type.name === "Referendum" ? (
+          election?.type.name
+        ) : (
+          <>Alegeri {election?.type.name}</>
+        )}
+        {" " + year}
+      </h2>
+      {route === "vote" && (
+        <Link
+          href={`/election/${election.electionId}`}
+          className={styles["link-results"]}
+        >
+          Rezultate{" "}
+        </Link>
       )}
-      {" " + year}
-    </h2>
+    </header>
   );
 };
 export default ElectionTitle;
